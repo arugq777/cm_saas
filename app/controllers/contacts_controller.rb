ee81332bf835e8303fa6d12c7e535ee6da46557c
gq <carnsarnit@gmail.com>
2015-04-11 23:20:55 -0500

    save contacts to database; rudimentary index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,27 @@
 class ContactsController < ApplicationController
 
+  def index
+    @contacts = Contact.all
+
+  end
+
   def new
     @contact = Contact.new
   end
 
   def create
-    @contact.save
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: 'saved!'
+    elsif
+      redirect_to new_contact_path, notice: 'NOPE.'
+    end
+        
   end
+
+  private 
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 
 end
