505c71416ae73d66fd946a7ba8d63fb312a7e068
gq <carnsarnit@gmail.com>
2015-04-14 09:49:46 -0500

    forgot to add an id to the submit button. Stripe form works now

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,34 +1,37 @@
 $(document).ready(function(){
-  Stripe.setPublishableKey($('meta[name="stripe-key:]').attr('content'));
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   //set up an event to watch for form submission
   $('#form-submit-btn').click(function(event){ 
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum    = $('#card_number').val(), 
-        cvcNum   = $('#card_code').val(), 
+    var ccNum = $('#card_number').val(), 
+        cvcNum = $('#card_code').val(), 
         expMonth = $('#card_month').val(), 
-        expYear  = $('#card_year').val();
+        expYear = $('#card_year').val();
     if (!error) { 
       //get the stripe token
-      Stripe.createToken({ 
-        number: ccNum,
-        cvc: cvcNum,
-        exp_month: expMonth,
-        exp_year: expYear
-      }, stripeResponseHandler);
+      var card = {
+        number: $('#card_number').val(), 
+        cvc: $('#card_code').val(), 
+        exp_month: $('#card_month').val(), 
+        exp_year: $('#card_year').val()
+        };
+      Stripe.createToken(card, stripeResponseHandler);
     }
     return false;
   }); //form submission
+
   function stripeResponseHandler(status, response){ 
+
     //get a reference to the form
-    var f = $('#new_user');
+    var f = $('.new_user');
 
     //get the token from the response
     var token = response.id;
 
     //Add token to the form:
-    f.append("<input type='hidden' name='user[stripe_card_token]' value='"+ token +"' />");
+    f.append("<input type='hidden' name='user[stripe_card_token]' value='" + token + "' />");
 
     //Submit form
     f.get(0).submit();
