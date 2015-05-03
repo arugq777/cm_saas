4482478429d898aa205e267b80673f2d095b5a43
gq <carnsarnit@gmail.com>
2015-05-03 01:17:45 -0500

    fixing stripe for heroku depployment

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,32 +1,31 @@
 $(document).ready(function(){
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   //set up an event to watch for form submission
-  $('.form-submit-btn').click(function(event){ 
+  $('.pro_form').submit(function(event){ 
+    var $form = $(this);
     event.preventDefault();
-    $('input[type=submit]').prop('disabled', true);
-    var error = false;
-      //get the stripe token
-    var card = {
-      number: $('#card_number').val(), 
-      cvc: $('#card_code').val(), 
-      exp_month: $('#card_month').val(), 
+    $('.form-submit-btn').prop('disabled', true);
+    Stripe.card.createToken({
+      number: $('#card_number').val(),
+      cvc: $('#card_code').val(),
+      exp_month: $('#card_month').val(),
       exp_year: $('#card_year').val()
-      };
-    Stripe.createToken(card, stripeResponseHandler);
+    }, stripeResponseHandler);
+
     return false;
   }); //form submission
 
   function stripeResponseHandler(status, response){ 
     //get a reference to the form
-    var f = $('.pro_form');
-
+    var $f = $('.pro_form');
+    //alert(response.id);
     //get the token from the response
     var token = response.id;
 
     //Add token to the form:
-    f.append($("<input type='hidden' name='user[stripe_card_token]'/>").val(token));
+    $f.append("<input type='hidden' name='user[stripe_card_token]' value='" + token + "'/>");
 
     //Submit form
-    f.get(0).submit();
+    $f.get(0).submit();
   }
 });
