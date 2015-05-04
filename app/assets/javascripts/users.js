f4a71301e3264814af3b2335b442e7e80fa25393
gq <carnsarnit@gmail.com>
2015-05-04 15:14:07 -0500

    set up seperate events for both stripe forms

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   //set up an event to watch for form submission
-  $('.pro_form').submit(function(event){ 
+  // $('.pro_form').submit(function(event){ 
+  //   var $form = $(this);
+  //   event.preventDefault();
+  //   $('.form-submit-btn').prop('disabled', true);
+  //   Stripe.card.createToken({
+  //     number: $('#card_number').val(),
+  //     cvc: $('#card_code').val(),
+  //     exp_month: $('#card_month').val(),
+  //     exp_year: $('#card_year').val()
+  //   }, stripeResponseHandler);
+
+  //   return false;
+  // }); //form submission
+
+  $('#pro_form').submit(function(event){
     var $form = $(this);
     event.preventDefault();
     $('.form-submit-btn').prop('disabled', true);
+  },stripeFormSubmit);
+
+  $('#pro_upgrade_form').submit(function(event){
+    var $form = $(this);
+    event.preventDefault();
+    $('.form-submit-btn').prop('disabled', true);
+  },stripeFormSubmit);
+
+  function stripeFormSubmit(){
     Stripe.card.createToken({
       number: $('#card_number').val(),
       cvc: $('#card_code').val(),
       exp_month: $('#card_month').val(),
       exp_year: $('#card_year').val()
     }, stripeResponseHandler);
-
-    return false;
-  }); //form submission
+    return false;    
+  }
 
   function stripeResponseHandler(status, response){ 
     //get a reference to the form
